09354c887717accab43d0eacf1995a6f47275392
Invitado <invitado@MacBook-Pro-de-IA.local>
2016-10-02 21:18:07 -0500

    mobile version test

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,19 @@
 (function (window) {
 	'use strict';
 
+	var Utils = Utils || {
+		create: function () {
+			var instance = Object.create(this);
+			return instance;
+		},
+		isMobile:function () {
+			return $(window).innerWidth() < 768;
+		}
+	}
+	window.Utils = Utils.create();
+
+
+
 	function flipDesk(type) {
 		$('.o-card--' + type + '-a').toggleClass('js-flip');
 
@@ -19,10 +32,32 @@
 		}
 	}
 
-	function flipMobile (type, info) {
-		$('.o-container').hide();
-		$('#' + info).show();
-		$('.o-card--' + type).toggleClass('js-flip');
+	function flipMobile (el) {
+		var bricks = $('.o-layout--mobile .o-brick'),
+				index = bricks.index(el);
+
+		el.toggleClass('js-flip');
+
+		if (index !== bricks.length - 1) {
+			var type = el.data('type');
+
+			$('.o-content').hide();
+			$('.o-content--' + type).show();
+			$('.o-layout--mobile').toggleClass('js-show-content');
+
+			if (index > 1 && index < 4) {
+				$('.o-layout--mobile').toggleClass('js-translate-up');
+				$('.o-curtain').removeAttr('style').css('top', 100 / 4 * 2 + '%');
+			} else if (index >= 4 && index < 6) {
+				$('.o-layout--mobile').toggleClass('js-translate-down');
+				$('.o-curtain').removeAttr('style').css('bottom', 100 / 4 * 2 + '%');
+			} else if (index <= 1) {
+				$('.o-curtain').removeAttr('style').css('top', 100 / 4 + '%');
+			} else if (index >= 6) {
+				$('.o-curtain').removeAttr('style').css('bottom', 100 / 4 + '%');
+			}
+		}
+
 	}
 
 	$('.o-brick').on('click', function (evt) {
@@ -30,9 +65,8 @@
 
 		var type = $(this).data('type');
 
-		if (type === 'full') {
-			var showInfo = $(this).data('show');
-			flipMobile(type, showInfo);
+		if (Utils.isMobile()) {
+			flipMobile($(this));
 		} else {
 			flipDesk(type);
 		}
