cfeebd3e72609a8b484164b6658b5739ace161bc
ftinoco <ftinoco@ia.com.mx>
2016-10-09 20:07:54 -0500

    casi remo

--- scripts/app.js
+++ scripts/app.js
@@ -1,84 +1,44 @@
-(function (window) {
-	'use strict';
+(function(global) {
+  'use strict';
 
-	var Utils = Utils || {
-		create: function () {
-			var instance = Object.create(this);
-			return instance;
-		},
-		isMobile:function () {
-			return $(window).innerWidth() < 768;
-		}
-	}
-	window.Utils = Utils.create();
+  var App = (function() {
 
+    var
+    /*Private*/
+    _platform = Utils.isMobile() ? 'mobile' : 'desktop',
+    _bricks = $('.o-layout--' + _platform + ' .o-brick'),
+    _caseBricks = $('.o-layout--' + _platform + ' .o-brick[data-type=case]'),
+    _renderData = function () {
+      _caseBricks.each(function (i, item) {
+        if (Service.cases[i]) {
+          var _case = Service.cases[i];
+          $(item)
+            .attr('data-slug', _case.slug)
+            .find('.o-card__face--back')
+            .css('background-image', 'url(' + _case.logo + ')');
+        }
+      });
+    },
 
+    /*Public*/
+    init = function () {
+      _renderData();
 
-	function flipDesk(type) {
-		$('.o-card--' + type + '-a').toggleClass('js-flip');
+      if (Utils.isMobile()) {
+        Mobile.init(_bricks);
+      } else {
+        Desktop.init(_bricks);
+      }
 
-		setTimeout(function () {
-			$('.o-card--' + type + '-b').toggleClass('js-flip');
-		}, 200);
+      Router.init();
+    };
 
-		if (type === "case" || type === "about") {
-			if ($('.o-card--crew-b').hasClass('js-hide')) {
-				setTimeout(function () {
-					$('.o-card--crew-b').toggleClass('js-hide');
-				}, 900);
-			} else {
-				$('.o-card--crew-b').toggleClass('js-hide');
-			}
-		}
-	}
+    return {
+      init: init
+    }
 
-	function flipMobile (el) {
-		var bricks = $('.o-layout--mobile .o-brick'),
-				index = bricks.index(el);
+  }());
 
-		el.toggleClass('js-flip');
+  global.App = App;
 
-		if (index !== bricks.length - 1) {
-			var type = el.data('type');
-
-			$('.o-content').hide();
-			$('.o-content--' + type).show();
-			$('.o-layout--mobile').toggleClass('js-show-content');
-
-			if (index > 1 && index < 4) {
-				$('.o-layout--mobile').toggleClass('js-translate-up');
-				$('.o-curtain').removeAttr('style').css('top', 100 / 4 * 2 + '%');
-			} else if (index >= 4 && index < 6) {
-				$('.o-layout--mobile').toggleClass('js-translate-down');
-				$('.o-curtain').removeAttr('style').css('bottom', 100 / 4 * 2 + '%');
-			} else if (index <= 1) {
-				$('.o-curtain').removeAttr('style').css('top', 100 / 4 + '%');
-			} else if (index >= 6) {
-				$('.o-curtain').removeAttr('style').css('bottom', 100 / 4 + '%');
-			}
-		}
-
-	}
-
-	$('.o-brick').on('click', function (evt) {
-		evt.preventDefault();
-
-		var type = $(this).data('type');
-
-		if (Utils.isMobile()) {
-			flipMobile($(this));
-		} else {
-			flipDesk(type);
-		}
-
-	});
-
-
-	$('.o-close').on('click', function (evt) {
-		evt.preventDefault();
-
-		$('.o-card').removeClass('js-flip');
-	});
-
-
-})(window); 
+}(window));
